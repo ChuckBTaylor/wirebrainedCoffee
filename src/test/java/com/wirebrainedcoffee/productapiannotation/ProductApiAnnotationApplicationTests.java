263f0a5e76b189f885b1ee9cd94f11e02257406f
ChuckBTaylor <WLScripps@gmail.com>
2020-05-11 09:45:53 +0300

    Initial commit

---
+++ src/test/java/com/wirebrainedcoffee/productapiannotation/ProductApiAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirebrainedcoffee.productapiannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
